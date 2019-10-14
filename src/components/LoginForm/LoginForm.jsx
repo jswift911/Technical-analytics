c09a15bd99b6b03cc113c121ebe3a9d1b86117fc
Alexey Kobevko <folco_2000@mail.ru>
2019-10-14 11:13:56 +0300

    мелкие правки

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -14,7 +14,7 @@ export const LoginForm = (props) => {
                     <input onChange={event => setUsername(event.target.value)} name="username" type="text" placeholder="username" value={username} />
                     <input onChange={event => setPassword(event.target.value)} name="password" type="password" placeholder="password" value={password} />
                     <button className={style.loginBtn} >sign in</button>
-                    <Link to={'/registration'}>registration</Link>
+                    <Link className={style.link} to={'/registration'}>Registration</Link>
                 </div>
             </div>
         </div>
